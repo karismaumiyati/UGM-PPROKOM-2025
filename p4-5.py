c25c69cea3a2aec92d7bfcbc9ab078b619fd3f3d
karismaumiyati <karismaumiyati@mail.ugm.ac.id>
2025-09-15 10:45:59 +0700

    Add files via upload

---
+++ p4-5.py
@@ -0,0 +1,33 @@
+nama_1 = (input ("Masukkan nama siswa 1:"))
+nilai_1 = int(input("Masukkan nilai siswa 1:"))
+nama_2 = (input ("Masukkan nama siswa 2:"))
+nilai_2 = int(input("Masukkan nilai siswa 2:"))
+nama_3 = (input ("Masukkan nama siswa 3:"))
+nilai_3 = int(input("Masukkan nilai siswa 3:"))
+nama_4 = (input ("Masukkan nama siswa 4:"))
+nilai_4 = int(input("Masukkan nilai siswa 4:"))
+nama_5 = (input ("Masukkan nama siswa 5:"))
+nilai_5 = int(input("Masukkan nilai siswa 5:"))
+print ("\nHASIL AKHIR KELULUSAN UJIAN SISWA:")
+
+if (nilai_1 >= 70):
+    print (nama_1, "lulus")
+else :
+    print (nama_1, "tidak lulus")
+if (nilai_2 >= 70):
+    print (nama_2, "lulus")
+else :
+    print (nama_2, "tidak lulus")
+if (nilai_3 >= 70):
+    print (nama_3, "lulus")
+else :
+    print (nama_3, "tidak lulus")
+if (nilai_4 >= 70):
+    print (nama_4, "lulus")
+else :
+    print (nama_4, "tidak lulus")
+if (nilai_5 >= 70):
+    print (nama_5, "lulus")
+else :
+    print (nama_5, "tidak lulus")
+
