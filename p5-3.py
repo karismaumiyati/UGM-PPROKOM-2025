2e498cf865c3e2847fc975e7cafbeae49b4b68c3
karismaumiyati <karismaumiyati@mail.ugm.ac.id>
2025-09-15 13:34:50 +0700

    Add files via upload

---
+++ p5-3.py
@@ -0,0 +1,9 @@
+jumlah_nilai = int(input("masukkan jumlah nilai:"))
+
+total = 0
+for i in range (jumlah_nilai):
+    nilai = float(input(f"Masukkan nilai ke- {i+1}:"))
+    total += nilai
+
+rata= total // jumlah_nilai
+print(f"Rata rata {jumlah_nilai} nilai: {rata} ")
