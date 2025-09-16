d757657cc179333394c232b500fde6f6f17eed50
karismaumiyati <karismaumiyati@mail.ugm.ac.id>
2025-09-16 15:26:02 +0700

    p5-3.py

--- p5-3.py
+++ p5-3.py
@@ -5,5 +5,6 @@ for i in range (jumlah_nilai):
     nilai = float(input(f"Masukkan nilai ke- {i+1}:"))
     total += nilai
 
-rata= total // jumlah_nilai
+rata= total / jumlah_nilai
+
 print(f"Rata rata {jumlah_nilai} nilai: {rata} ")
